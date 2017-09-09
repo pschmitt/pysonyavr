fa1914232e514130e318e75805c2c1ca542d67c7
Philipp Schmitt <philipp@schmitt.co>
2017-09-09 20:38:03 +0200

    Version 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='pysonyavr',
-    version='1.0',
+    version='1.1',
     license='GPL3',
     description='Python bindings for modern Sony speakers',
     long_description=read('README.rst'),
